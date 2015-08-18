95ae171378a1df7d0c7dc123dac8c251f8bd0777
Heart Beat Studio <blacklight728@gmail.com>
2015-08-18 15:25:39 -0400

    Exercise 1 answer

---
+++ exercise_1.rb
@@ -0,0 +1,24 @@
+puts 2
+puts 3
+puts 2 != 3
+puts
+puts 'Hello world'
+puts "Hello world"
+puts
+puts "Betty's pie shop"
+puts 'Betty\'s pie shop'
+puts
+puts "Enter name"
+name = gets.chomp
+puts "Your name is #{name}"
+puts
+a = 20
+b = 65
+puts "The value of a is #{a}." # The value of a is 20.
+puts "The value of a is #{b}." # The value of a is 65.
+puts "The value of a plus b is #{a + b}." # The value of a plus b is 85.
+puts
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
